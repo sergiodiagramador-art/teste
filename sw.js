8cde2475d1ee7b616578d5d2f8bebace7670b6ab
sergiodiagramador-art <sergiodiagramador@gmail.com>
2026-09-09 23:25:54 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,5 @@
+const CACHE_NAME = 'sosrim-v3.2';
+const ASSETS = ['./', './index.html', './manifest.json', './icons/ambulance.svg'];
+self.addEventListener('install', event => { self.skipWaiting(); event.waitUntil(caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))); });
+self.addEventListener('activate', event => { event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))).then(() => self.clients.claim()))); });
+self.addEventListener('fetch', event => { event.respondWith(fetch(event.request).catch(() => caches.match(event.request))); });
